5350ba986b0b6ed63e94eb2b35f0f9d2e36709d6
Marcello <marcellokabora@gmail.com>
2025-05-09 10:27:34 +0200

    wip

--- components/Card.vue
+++ components/Card.vue
@@ -31,47 +31,57 @@ function onTimes(value: number) {
 </script>
 
 <template>
-    <div class="product">
-        <div class="container">
+    <div class="product bg-white rounded-xl shadow-md relative transition-all duration-200">
+        <div class="container text-center flex flex-col items-center justify-between gap-4 p-8">
             <NuxtLink :to="'/product/' + product.id">
-                <NuxtImg :src="product.thumbnail" :alt="product.id" />
-                <div class="title">{{ product.title }}</div>
+                <NuxtImg :src="product.thumbnail" :alt="product.id"
+                    class="w-auto h-36 mx-auto transition-all duration-500 object-contain" />
+                <div class="title font-medium h-12 overflow-hidden flex items-center justify-center">
+                    {{ product.title }}
+                </div>
             </NuxtLink>
-            <div v-if="!isCart" class="infos" @click="addCart(product)">
+            <div v-if="!isCart"
+                class="infos flex items-center justify-center gap-4 bg-primary w-full rounded-xl p-4 text-white mb-[-1em] cursor-pointer transition-all duration-200 hover:shadow-md">
                 <div>Buy</div>
-                <div class="info">
+                <div class="info flex items-center gap-1">
                     <span>{{ product.price }}</span>
                     <Icon name="material-symbols:euro" />
                 </div>
             </div>
-            <div v-else class="times">
-                <div class="info price">
+            <div v-else class="times flex items-center justify-center flex-col gap-4 w-full p-4 mb-[-1em]">
+                <div class="info price font-bold text-lg flex items-center gap-1">
                     <div>Price</div>
-                    <div class="info">
+                    <div class="info flex items-center gap-1">
                         <span>{{ product.price }}</span>
                         <Icon name="material-symbols:euro" />
                     </div>
                 </div>
-                <div class="info more">
+                <div class="info more flex items-center gap-4">
                     <UButton icon="material-symbols:remove-rounded" @click="onTimes(-1)" :disabled="times === 1" />
                     <span>{{ times }}</span>
                     <UButton icon="material-symbols:add" @click="onTimes(1)" />
                 </div>
             </div>
         </div>
-        <div class="actions">
+        <div class="actions absolute top-4 right-4">
             <div v-if="isCart || isFavo">
-                <button class="favorite" title="Remove" @click="onRemove">
+                <button
+                    class="favorite border rounded-full h-10 w-10 flex items-center justify-center border-gray-300 cursor-pointer"
+                    title="Remove" @click="onRemove">
                     <Icon name="material-symbols:delete" />
                 </button>
             </div>
-            <div v-else class="block">
-                <button class="favorite" :class="{ active: product?.favorite }" title="Favorite"
+            <div v-else class="grid gap-4">
+                <button
+                    class="favorite border rounded-full h-10 w-10 flex items-center justify-center border-gray-300 cursor-pointer"
+                    :class="{ 'bg-red-400 text-white': product?.favorite }" title="Favorite"
                     @click="onFavorite(product)">
                     <Icon name="material-symbols:favorite" />
                 </button>
                 <UModal>
-                    <button class="favorite preview" title="Preview">
+                    <button
+                        class="favorite preview border rounded-full h-10 w-10 flex items-center justify-center border-gray-300 cursor-pointer"
+                        title="Preview">
                         <Icon name="material-symbols:visibility" />
                     </button>
                     <template #content>
@@ -84,131 +94,6 @@ function onTimes(value: number) {
 </template>
 
 <style scoped>
-.product {
-    text-decoration: none;
-    background-color: white;
-    margin: 0;
-    border-radius: 1em;
-    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
-    position: relative;
-    transition: all 0.2s;
-}
-
-.container {
-    text-align: center;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    text-align: center;
-    justify-content: space-between;
-    gap: 1em;
-    padding: 2em;
-
-    .title {
-        font-weight: 500;
-        height: 50px;
-        overflow: hidden;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-
-    img {
-        width: auto;
-        height: 150px;
-        margin: auto;
-        transition: all 0.5s;
-        object-fit: contain;
-    }
-}
-
-.infos {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1em;
-    background-color: var(--ui-primary);
-    width: 100%;
-    border-radius: 1em;
-    padding: 1em;
-    color: white;
-    margin-bottom: -1em;
-    cursor: pointer;
-    transition: all .2s;
-
-    &:hover {
-        box-shadow: 0px 0px 0px 0.1em var(--color-secondary);
-    }
-
-    .info {
-        display: flex;
-        align-items: center;
-        gap: 0.3em;
-    }
-}
-
-.times {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    gap: 1em;
-    width: 100%;
-    padding: 1em;
-    margin-bottom: -1em;
-
-    .info {
-        display: flex;
-        align-items: center;
-        gap: 0.3em;
-
-        &.price {
-            font-weight: bold;
-            font-size: 1.2em;
-        }
-
-        &.more {
-            gap: 1em;
-        }
-
-        button {
-            cursor: pointer;
-        }
-    }
-}
-
-.actions {
-    position: absolute;
-    top: 1em;
-    right: 1em;
-
-    .block {
-        display: grid;
-        gap: 1em;
-    }
-}
-
-.favorite {
-    border-radius: 100px;
-    border: none;
-    cursor: pointer;
-    height: 40px;
-    width: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid silver;
-
-    &.active {
-        background-color: var(--ui-primary);
-        color: white;
-    }
-
-    &:hover:not(.active) {
-        color: var(--ui-primary);
-    }
-}
-
 @media (width<1000px) {
     .preview {
         display: none;
